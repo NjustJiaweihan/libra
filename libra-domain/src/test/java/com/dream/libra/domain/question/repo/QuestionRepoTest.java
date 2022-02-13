8d44d5905088a3f4d33755de71161df14c6338ad
NjustJiaweihan <1390445546@qq.com>
2022-02-13 15:18:40 +0800

    junit test

---
+++ libra-domain/src/test/java/com/dream/libra/domain/question/repo/QuestionRepoTest.java
@@ -0,0 +1,12 @@
+package com.dream.libra.domain.question.repo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionRepoTest {
+
+    @Test
+    public void get() {
+    }
+}
---
+++ libra-domain/src/test/java/com/dream/libra/domain/question/service/QuestionServiceTest.java
@@ -0,0 +1,12 @@
+package com.dream.libra.domain.question.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionServiceTest {
+
+    @Test
+    public void get() {
+    }
+}
